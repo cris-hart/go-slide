bdc495c833c736e72648f5bea971b8a5eab86680
Eithel <tux.eithel@gmail.com>
2015-07-27 22:32:14 +0200

    Added type Parrot

--- animal/animal.go
+++ animal/animal.go
@@ -7,3 +7,12 @@ type Animal struct {
 func (h *Animal) MyNameIs() string {
 	return h.Name
 }
+
+type Parrot struct {
+	Animal
+	Phrase string
+}
+
+func (p *Parrot) Speak() string {
+	return p.Phrase
+}
